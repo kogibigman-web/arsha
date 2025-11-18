6477cf9c8c3e9525dc6df4fdfb489fa802b7034d
unknown <kogibigman@gmail.com>
2025-11-18 13:14:24 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+let number = 1;
+console.log(number);
+
+let name = "rykid";
+console.log(name);
+
+let person = name;
+console.log(person);
+
+let num1 = 5;
+let num2 = 3;
+let num3 = 10;
+let result = num1 + num3 * num2;
+console.log(result);
+
+let result2 = (num1 + num3) / num2;
+console.log(result2);
+
